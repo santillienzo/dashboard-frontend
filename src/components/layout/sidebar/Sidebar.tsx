dbe47c1dff659dcd27a1af0a86185de652f9ac4d
santillienzo <enzo135246@gmail.com>
2024-03-09 03:13:12 -0300

    feat: se agrego material ui. se agrego el selector en empresa

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,26 +1,40 @@
-import { IconHome } from '@tabler/icons-react';
+import { IconBuildingStore, IconChevronDown, IconChevronUp, IconHome } from '@tabler/icons-react';
 import { IconArchive, IconBuilding, IconCalendarMonth, IconChartPie, IconCreditCard, IconPhone, IconSettings, IconUserCircle } from '@tabler/icons-react'
 // import { useNavigate } from 'react-router-dom';
-import { Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material';
+import { Collapse, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { Fragment, useState } from 'react';
 
 interface ILinkItem {
   title: string;
   path: string;
   icon: JSX.Element;
-  selected?: boolean;
+  innerLinks?: ILinkItem[]
+  open?: boolean;
 }
 
-const links: ILinkItem[] = [
+const firstLinks: ILinkItem[] = [
   {
     title: 'Inicio',
     path: '/',
     icon: <IconHome />,
-    selected: true
   },
   {
     title: 'Empresa',
     path: '/empresa',
-    icon: <IconBuilding />
+    icon: <IconBuilding />,
+    innerLinks: [
+      {
+        icon: <IconSettings/>,
+        title: 'Configuración',
+        path: '/empresa/configuracion',
+      },
+      {
+        icon: <IconBuildingStore/>,
+        title: 'Sucursal/es',
+        path: '/empresa/sucursales',
+      }
+    ],
   },
   {
     title: 'Productos',
@@ -67,7 +81,28 @@ const otherLinks: ILinkItem[] = [
   }
 ]
 
-const Sidebar = () => {  
+const Sidebar = () => {
+  const [links, setLinks] = useState<ILinkItem[]>(firstLinks)
+
+  const navigator = useNavigate()
+  const handleRedirect = (path:string)=>{
+    navigator(path)
+  }
+
+  const togleOpenLinks = (title:string)=>{
+    setLinks(links.map(link =>{
+      if (link.title === title) {
+        console.log(link.open)
+        return {
+          ...link,
+          open: !link.open
+        }
+      }
+
+      return link
+    }))
+  }
+
   return (
     <Drawer
       sx={{
@@ -85,20 +120,41 @@ const Sidebar = () => {
       <Divider />
       <List>
         {links.map((link, index) => (
-          <ListItem key={index} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {link.icon}
-              </ListItemIcon>
-              <ListItemText primary={link.title} />
-            </ListItemButton>
-          </ListItem>
+          <Fragment key={index}>
+            <ListItem disablePadding onClick={()=> link.innerLinks ? togleOpenLinks(link.title) : handleRedirect(link.path)}>
+              <ListItemButton>
+                <ListItemIcon>
+                  {link.icon}
+                </ListItemIcon>
+                <ListItemText primary={link.title} />
+                {link.innerLinks ?  link.open ? <IconChevronDown /> : <IconChevronUp /> : null}
+              </ListItemButton>
+            </ListItem>
+            {
+              link.innerLinks && <Collapse in={link.open} timeout="auto" unmountOnExit>
+                <List>
+                  {
+                    link.innerLinks.map((innerLink, index) => (
+                      <ListItem key={index} sx={{ pl: 2 }} disablePadding onClick={()=> handleRedirect(innerLink.path)}>
+                        <ListItemButton>
+                          <ListItemIcon>
+                            {innerLink.icon}
+                          </ListItemIcon>
+                          <ListItemText primary={innerLink.title} />
+                        </ListItemButton>
+                      </ListItem>
+                    ))
+                  }
+                </List>
+              </Collapse>
+            }
+          </Fragment>
         ))}
       </List>
       <Divider />
       <List>
         {otherLinks.map((link, index) => (
-          <ListItem key={index} disablePadding>
+          <ListItem key={index} disablePadding onClick={()=> link.innerLinks ? togleOpenLinks(link.title) : handleRedirect(link.path)}>
             <ListItemButton>
               <ListItemIcon>
                 {link.icon}
@@ -109,32 +165,6 @@ const Sidebar = () => {
         ))}
       </List>
     </Drawer>
-    // <nav className={style.sidebar}>
-    //   <div className={style.logoContainer}>
-    //     {/* <img src={logo} alt="logo" className={style.logo} /> */}
-    //     <h5>Logo Empresa</h5>
-    //   </div>
-    //   <ul className={style.principalLinks}>
-    //     {
-    //       links.map((link, i) =>(
-    //         <LinkItem 
-    //           key={i} 
-    //           title={link.title} 
-    //           icon={link.icon} 
-    //           path={link.path}
-    //           selected={link.selected}
-    //         />
-    //       ))
-    //     }
-    //   </ul>
-    //   <ul className={style.otherLinks}>
-    //     {
-    //       otherLinks.map((link,i) =>(
-    //         <LinkItem key={i} title={link.title} icon={link.icon} path={link.path}/>
-    //       ))
-    //     }
-    //   </ul>
-    // </nav>
   )
 }
 
